1275ce6a6fafbc5069e4c0192eac6309141f07c6
conectabrasilssh <40015559+conectabrasilssh@users.noreply.github.com>
2018-09-12 00:06:46 -0300

    Create Plus.sh

---
+++ Plus.sh
@@ -0,0 +1,148 @@
+1
+wget https://www.dropbox.com/s/qx9avo9lvppo26d/addhost -O /bin/addhost
+chmod +x /bin/addhost
+
+
+2
+wget https://www.dropbox.com/s/ffnfr6zevq7zjyq/ajuda -O /bin/ajuda
+chmod +x /bin/ajuda
+
+
+3
+wget https://www.dropbox.com/s/5q5boyrjt5bjbqw/alterarlimite -O /bin/alterarlimite
+chmod +x /bin/alterarlimite
+
+
+4
+wget https://www.dropbox.com/s/hsfdnd2207zj779/alterarsenha -O /bin/alterarsenha
+chmod +x /bin/alterarsenha
+
+wget https://www.dropbox.com/s/emhvd66qnh94xmo/attscript -O /bin/attscript
+chmod +x /bin/attscript
+
+wget https://www.dropbox.com/s/fbr3q5uslzwvi15/badvpn -O /bin/badvpn
+chmod +x /bin/badvpn
+
+wget https://www.dropbox.com/s/w6n070a2vgbuxmi/banner -O /bin/banner
+chmod +x /bin/banner
+
+wget https://www.dropbox.com/s/xfir3ufvp7od06o/blockt -O /bin/blockt
+chmod +x /bin/blockt
+
+wget https://www.dropbox.com/s/njqliah8hegqsx8/botssh -O /bin/botssh
+chmod +x /bin/botssh
+
+wget https://www.dropbox.com/s/8ubwxbghdtne21i/chacl -O /bin/chacl
+chmod +x /bin/chacl
+
+wget https://www.dropbox.com/s/2qc3ninf3fbqwjk/conexao -O /bin/conexao
+chmod +x /bin/conexao
+
+wget https://www.dropbox.com/s/ljlb28rjcn4mva7/criarteste -O /bin/criarteste
+chmod +x /bin/criarteste
+
+wget https://www.dropbox.com/s/jty078s76oxbcaq/criarusuario -O /bin/criarusuario
+chmod +x /bin/criarusuario
+
+wget https://www.dropbox.com/s/v4nkglswbq4yua4/delhost -O /bin/delhost
+chmod +x /bin/delhost
+
+wget https://www.dropbox.com/s/l7mnpl7vnpb7gp3/delscript -O /bin/delscript
+chmod +x /bin/delscript
+
+wget https://www.dropbox.com/s/fj33qaculnx2sm3/detalhes -O /bin/detalhes
+chmod +x /bin/detalhes
+
+wget https://www.dropbox.com/s/7q5jbdfaeht9dc1/detalhesusers -O /bin/detalhesusers
+chmod +x /bin/detalhesusers
+
+wget https://www.dropbox.com/s/6k5nidix88kldm0/droplimiter -O /bin/droplimiter
+chmod +x /bin/droplimiter
+
+wget https://www.dropbox.com/s/x3mxyhqytcdy5mi/expcleaner -O /bin/expcleaner
+chmod +x /bin/expcleaner
+
+wget https://www.dropbox.com/s/d1up3kjhmkofhzi/getfacl -O /bin/getfacl
+chmod +x /bin/getfacl
+
+wget https://www.dropbox.com/s/trkq4kdwuej3zuo/infousers -O /bin/infousers
+chmod +x /bin/infousers
+
+wget https://www.dropbox.com/s/n5vsftk4l5fpf6b/limite -O /bin/limite
+chmod +x /bin/limite
+
+wget https://www.dropbox.com/s/ac05qplkgr3r6hu/limiter -O /bin/limiter
+chmod +x /bin/limiter
+
+wget https://www.dropbox.com/s/rhbuoplkpiqa8sj/loginctl -O /bin/loginctl
+chmod +x /bin/loginctl
+
+wget https://www.dropbox.com/s/c31v8d0fonfphdp/menu -O /bin/menu
+chmod +x /bin/menu
+
+wget https://www.dropbox.com/s/mtln17xgn8rhc1m/mudardata -O /bin/mudardata
+chmod +x /bin/mudardata
+
+wget https://www.dropbox.com/s/kuxdmpewfbnwl6b/openvt -O /bin/openvt
+chmod +x /bin/openvt
+
+wget https://www.dropbox.com/s/88qtzl7iork1p74/otimizar -O /bin/otimizar
+chmod +x /bin/otimizar
+
+wget https://www.dropbox.com/s/ui5ddg55wb9f46a/ovpn -O /bin/ovpn
+chmod +x /bin/ovpn
+
+wget https://www.dropbox.com/s/r26qoq951r66p5q/payload -O /bin/payload
+chmod +x /bin/payload
+
+wget https://www.dropbox.com/s/0gmdkrw7puhlvi2/reiniciarservicos -O /bin/reiniciarservicos
+chmod +x /bin/reiniciarservicos
+
+wget https://www.dropbox.com/s/kbtj2ufb65vpssw/reiniciarsistema -O /bin/reiniciarsistema
+chmod +x /bin/reiniciarsistema
+
+wget https://www.dropbox.com/s/fbnynlgez0tpjlv/remover -O /bin/remover
+chmod +x /bin/remover
+
+wget https://www.dropbox.com/s/4h2gwci22a1rqox/senharoot -O /bin/senharoot
+chmod +x /bin/senharoot
+
+wget https://www.dropbox.com/s/ry2flynqcufurj3/setfacl -O /bin/setfacl
+chmod +x /bin/setfacl
+
+wget https://www.dropbox.com/s/fce4y68vy68vajq/socks -O /bin/socks
+chmod +x /bin/socks
+
+wget https://www.dropbox.com/s/mx0ne3jhb8fy9t1/speedtest -O /bin/speedtest
+chmod +x /bin/speedtest
+
+wget https://www.dropbox.com/s/v7s7ikyzi3ks804/sshmonitor -O /bin/sshmonitor
+chmod +x /bin/sshmonitor
+
+wget https://www.dropbox.com/s/inuhan7xerfn9rd/TCP -O /bin/TCP
+chmod +x /bin/TCP
+
+wget https://www.dropbox.com/s/j00hnugkt9ybc7k/tcpspeed -O /bin/tcpspeed
+chmod +x /bin/tcpspeed
+
+wget https://www.dropbox.com/s/z2yqefe32lfael8/testador -O /bin/testador
+chmod +x /bin/testador
+
+wget https://www.dropbox.com/s/eut6x0kgohhye1b/userbackup -O /bin/userbackup
+chmod +x /bin/userbackup
+
+wget https://www.dropbox.com/s/pnbg6o3gyyd61m7/versao -O /bin/versao
+chmod +x /bin/versao
+
+wget https://www.dropbox.com/s/qhxyk84epbfkbno/IP -O /etc/IP
+chmod +x /etc/IP
+
+cd /etc
+
+wget https://www.dropbox.com/s/92g1w19ciwbp77f/SSHPLUS.zip unzip SSHPLUS.zip && rm SSHPLUS.zip
+history -c && history -w
+
+
+
+
+
